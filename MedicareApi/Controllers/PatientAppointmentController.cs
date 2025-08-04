36743da6699c1c137f7565a2c28e49840f9084a2
Ameer Kulabi <ameerhaider1394@gmail.com>
2025-08-04 19:29:02 +0200

    Retrieve patient appointments

---
+++ MedicareApi/Controllers/PatientAppointmentController.cs
@@ -0,0 +1,60 @@
+﻿using MedicareApi.Data;
+using MedicareApi.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace MedicareApi.Controllers
+{
+    [ApiController]
+    [Route("api/patient/appointments")]
+    [Authorize]
+    public class PatientAppointmentController : ControllerBase
+    {
+        private readonly ApplicationDbContext _db;
+
+        public PatientAppointmentController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetPatientAppointments([FromQuery] String type)
+        {
+            var userId = User.FindFirst("uid")?.Value;
+            var isDoctor = User.FindFirst("isDoctor")?.Value == "True";
+            if (isDoctor) return Unauthorized();
+
+            var appointments = new List<Appointment>();
+            if(type == "past")
+                appointments = await _db.Appointments.Where(a => a.PatientId == userId && a.ScheduledAt < DateTime.Now).ToListAsync();
+            else
+                appointments = await _db.Appointments.Where(a => a.PatientId == userId && a.ScheduledAt > DateTime.Now).ToListAsync();
+            var patientAppointments = new List<PatientAppointment>();
+            foreach( Appointment appointment in appointments )
+            {
+                var doctor = await _db.Doctors.FirstAsync(a => a.Id == appointment.DoctorId);
+                patientAppointments.Add(new PatientAppointment
+                {
+                    id = appointment.Id,
+                    doctorName = doctor.Name,
+                    doctorSpecialization = doctor.Specialization,
+                    clinicName = doctor.ClinicName,
+                    date = appointment.ScheduledAt.Date.ToShortDateString(),
+                    time = appointment.ScheduledAt.TimeOfDay.ToString(),
+                    duration = 30,
+                    reason = "TBD",
+                    status = appointment.Status,
+                    type = "consultation",
+                    address = doctor.ClinicAddress,
+                    phone = doctor.Phone,
+                    consultationFee = int.Parse(doctor.ConsultationFee),
+                });
+            }
+            
+            return Ok(patientAppointments);
+
+
+        }
+    }
+}
---
+++ MedicareApi/Models/PatientAppointment.cs
@@ -0,0 +1,20 @@
+﻿namespace MedicareApi.Models
+{
+    public class PatientAppointment
+    {
+        public string id { get; set; }
+        public string doctorName { get; set; }
+        public string doctorSpecialization { get; set; }
+        public string clinicName { get; set; }
+        public string date {  get; set; }
+        public string time { get; set; }
+        public int duration { get; set; }
+        public string reason { get; set; }
+        public string status { get; set; }
+        public string type { get; set; }
+        public string phone { get; set; }
+        public string address { get; set; }
+        public int consultationFee { get; set; }
+
+    }
+}
