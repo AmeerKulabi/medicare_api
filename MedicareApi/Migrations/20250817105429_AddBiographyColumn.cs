eae82c7d61236a888de7a5668cf15d189c2c0fe6
Ameer Kulabi <ameerhaider1394@gmail.com>
2025-08-17 15:41:14 +0300

    Add new column to the doctor table

---
+++ MedicareApi/Migrations/20250817105429_AddBiographyColumn.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MedicareApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddBiographyColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ProfessionalBiography",
+                table: "Doctors",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ProfessionalBiography",
+                table: "Doctors");
+        }
+    }
+}
